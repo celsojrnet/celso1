927ffc88d3d701fee2fabea64e2b1359fa8a2b10
celsojrnet <celsojrnet78@gmail.com>
2016-04-03 18:39:31 -0700

    added items

--- add-content.js
+++ add-content.js
@@ -4,11 +4,11 @@ var hourNow = today.getHours();
 var year = today.getFullYear();
 var greeting;
 
-if (hourNow => 18) {
+if (hourNow > 18) {
   greeting = 'Good Evening!';
 } else if (hourNow > 12) {
   greeting = 'Good Afternoon!';
-} else if (hourNow => 0) {
+} else if (hourNow > 0) {
   greeting = 'Good Morning!';
 } else {
   greeting = 'Welcome!';
@@ -23,9 +23,9 @@ $(".horizon-swiper > div:gt(0)").hide();
 
 setInterval(function() {
 $('.horizon-swiper > div:first')
-.slideToggle(1000)
+.slideToggle(2000)
 .next()
 .fadeIn(3000)
 .end()
 .appendTo('.horizon-swiper');
-},  4000);
+},  7000);
